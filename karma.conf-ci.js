25f802aaa5e1ac06725af7f3c68d466ebb9449b2
Julian Motz <me@julianmotz.com>
2017-01-06 21:26:07 +0100

    Record SL video

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -182,7 +182,9 @@ module.exports = config => {
             "karma-coverage"
         ],
         sauceLabs: {
-            testName: "mark.js unit tests"
+            testName: "mark.js unit tests",
+            recordVideo: true,
+            recordScreenshots: true
         },
         customLaunchers: customLaunchers,
         browsers: Object.keys(customLaunchers),
